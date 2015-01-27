23e36c7092c32ff86f04b14ac31b193e53fb7bf7
nacho <nacho@xeonserver.futbolfanatics.com>
2015-01-27 05:16:11 -0500

    Subiendo generacion de poblacion inicial

--- pvz.cpp
+++ pvz.cpp
@@ -3,42 +3,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <algorithm> 
 #include "mtrand.h"
 #include "constants.h"
 
 using namespace std;
 
+MTRand_int32 mtrand;
+
+void init(){
+	struct timeval tim;  
+	gettimeofday(&tim, NULL);
+	mtrand.seed(1000000 * tim.tv_sec + tim.tv_usec);
+}
+
 char toSingleChar(int g){
 	return (char)(((int)'0')+g);
 }
 
-char randPlant(int g){	
-	int v = g % MAX_ELEMENT_TYPE;
+char randPlant(){	
+	int v = mtrand() % MAX_ELEMENT_TYPE;
 	return toSingleChar(v < 0 ? -1 * v : v);
 }
 
-std::vector<string> generarPoblacion(
-	MTRand_int32 gen,
-	int h){
-	
+std::vector<string> generarPoblacion(int h){
 	int ml = h * MAX_FILAS;
 	vector<string> v = vector<string>();
+	char s[ml];
+	
 	for(int i = 0; i < INDIVIDUOS_POR_POBLACION; ++i ){
-		cout<<i<<endl;
+		s[ml] = '\0';
+		generate_n (s, ml, randPlant);
+		v.push_back(s);
 	}
+	return v;
 }
 
-int main(){
-	struct timeval tim;  
-	gettimeofday(&tim, NULL);
-	MTRand_int32 gen(1000000 * tim.tv_sec + tim.tv_usec);
+int main(){	
 	
-	char c;	
-	for (int i = 0; i < 10; ++i)
-	{
-		c = randPlant(gen());		
-		cout << c <<endl;
-	}
+	init();
+
+	vector<string> init_pob = generarPoblacion(2);
+
+	for (vector<string>::iterator it = init_pob.begin(); it != init_pob.end(); ++it)
+	    cout << '\t' << *it;
+	cout << '\n';
+
+	cout<< init_pob.size() <<endl;
 
 	return EXIT_SUCCESS;
 }
