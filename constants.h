104ff0fa12c268c6e76fd2f01385b6a18fdc869f
nacho <nacho@xeonserver.futbolfanatics.com>
2015-01-27 21:35:02 -0500

    Subiendo carga de archivo

--- constants.h
+++ constants.h
@@ -1,13 +1,14 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-extern const int MAX_FILAS = 4;
+extern const int MAX_COLUMNAS = 4;
 extern const int MAX_VIDA_ZOMBIE = 4;
 extern const int MAX_VIDA_PLANTA = 1;
 extern const int MAX_PASO_ZOMBIE = 1;
 extern const int MAX_ELEMENT_TYPE = 4;
 extern const int INDIVIDUOS_POR_POBLACION = 100;
-extern const string INPUT_FILE = "input.in";
+extern const int MAX_GENERACIONES = 50;
 
+enum PLANTA { VACIO, PEASHOTTER, REPEATER, POTATO_MINE };
 
 #endif // CONSTANTS_H
--- pvz.cpp
+++ pvz.cpp
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
-#include <algorithm> 
+#include <fstream> 
+#include <algorithm>
 #include "mtrand.h"
 #include "constants.h"
 
@@ -11,7 +12,39 @@ using namespace std;
 
 MTRand_int32 mtrand;
 
-void loadEntry(){}
+void loadEntry(
+	int &w, 
+	int &h, 
+	int &z, 
+	vector<int> &zombie_vector, 
+	string filename){
+
+	fstream f;
+	f.open(filename.c_str(), std::fstream::in);
+	int zombie;
+	if(f){
+		f >> w >> h >> z;
+
+		if (h < MAX_COLUMNAS || h == MAX_COLUMNAS)
+		{
+			f.close();
+			cout<< "\tNo existe suficiente espacio"<<endl<<"\tpara colocar las plantas"<<endl<<"\tdentro de una matriz "<<w<<" x "<<h<<endl;
+			cout<< "\tDebe existir espacio para una"<<endl<<"\tmatriz "<<w<<" x "<<MAX_COLUMNAS<<endl;
+			exit(EXIT_FAILURE);
+		}
+
+		for (int i = 0; i < z && !f.eof(); ++i)
+		{
+			f>>zombie;
+			zombie_vector.push_back(zombie);
+		}
+		f.close();
+	}else{
+		cout<<"Problemas abriendo archivo de entrada \""<< filename <<"\""<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+}
 
 void init(){
 	struct timeval tim;  
@@ -19,13 +52,23 @@ void init(){
 	mtrand.seed(1000000 * tim.tv_sec + tim.tv_usec);
 }
 
-void printMatrix(string x, int r, int c){
+void printMatrix(string x, int r, int c){	
 	for(int i=0; i<r; i++){
+		cout << "[";
 		for(int j=0; j<c; j++){
 			cout<<" "<<x[i*c+j];
 		}
-		cout<<endl;
+		cout<<" ]"<<endl;
 	}
+}
+
+template <class T>
+void printVector(std::vector<T> zv)
+{
+	for(int i=0;i<zv.size();++i)
+		cout<<zv[i]<<" ";
+	cout<<endl;
+
 }
 
 char toSingleChar(int g){
@@ -37,8 +80,8 @@ char randPlant(){
 	return toSingleChar(v < 0 ? -1 * v : v);
 }
 
-std::vector<string> generarPoblacion(int h){
-	int ml = h * MAX_FILAS;
+vector<string> generarPoblacion(int h){
+	int ml = h * MAX_COLUMNAS;
 	vector<string> v = vector<string>();
 	char s[ml];
 	
@@ -53,15 +96,21 @@ std::vector<string> generarPoblacion(int h){
 int main(){	
 	
 	init();
+	int w, h, z, g = 0;
+	vector<int> zv = vector<int>();
+	loadEntry(w, h, z, zv, "input.in");
+	vector<string> init_pob;
 
-	vector<string> init_pob = generarPoblacion(2);
+	while(g++ < MAX_GENERACIONES){
+		init_pob = generarPoblacion(w);
+		printMatrix(init_pob[0], w, MAX_COLUMNAS);
+	}
 
-	for (vector<string>::iterator it = init_pob.begin(); it != init_pob.end(); ++it)
-	    cout << '\t' << *it;
-	cout << '\n';
-
-	printMatrix(init_pob[0],4,2);
-	cout<< init_pob.size() <<endl;
-
+	//printVector<int>(zv);
+	//printMatrix(init_pob[0],4,4);
+	//PLANTA p = VACIO;
+	//cout << p <<endl;
+	
 	return EXIT_SUCCESS;
+
 }
